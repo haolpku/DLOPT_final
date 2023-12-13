68b8f12559a4e826e9e13c6a5d3209a123e4dbf8
zllin <145895044+zl1in@users.noreply.github.com>
2023-12-13 23:45:15 +0800

    更新Non_convex.py

--- Non_convex.py
+++ Non_convex.py
@@ -1,108 +1,100 @@
 import torch
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.animation import FuncAnimation
-from Optimizers import * 
 from utils import *
+
+# 定义非凸函数（双曲抛物面）
+def h(x, y):
+    return x**2 - y**2
+
+# 定义一个有极小点的非凸函数
+def g(x, y):
+    return (x**2 - 10) ** 2 + (y**2 - 10) ** 2 + 20*x + 20*y
+
+def optimize_function_with_optimizers(optimize_fn, optimizer_name, lr=0.01, num_iterations=50, init_point=[0., 0.]):
+    '''
+    通用的优化函数，用于优化给定的函数
+    
+    参数:
+    optimize_fn - 要优化的函数
+    optimizer_class - 使用的优化器类
+    lr - 学习率
+    num_iterations - 优化步数
+    init_point - 优化的初始点
+    
+    返回:
+    history - 参数在优化过程中的历史值
+    '''  
+    # 设置设备
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # 选择优化器
+    optimizers = {
+    'SGD': SGDOptimizer,
+    'MomentumSGD': MomentumSGDOptimizer,
+    'AdaGrad': AdagradOptimizer,
+    'RMSProp': RMSPropOptimizer,
+    'Adam': AdamOptimizer
+    }    
+    optimizer_class = optimizers[optimizer_name]
+    # 初始化参数
+    params = [torch.tensor(init_point, requires_grad=True, device=device)]
+    optimizer = optimizer_class(params, lr=lr)
+    # 记录优化过程中的参数值
+    history = []
+    for i in range(num_iterations):
+        optimizer.zero_grad()
+        # 展开 params 并传递给优化函数
+        loss = optimize_fn(params[0][0], params[0][1])
+        loss.backward()
+        optimizer.step()
+        history.append([p.detach().clone().cpu().numpy() for p in params])
+        if (i + 1) % 10 == 0:
+            print(f"Optimizer: {optimizer_name}, Iteration: {i+1}/{num_iterations}, Loss: {loss.item()}")
+    return history
 
 # 固定随机种子
 seed_everything()
 
-# 设置设备
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# 初始设置
+lr = 0.01
+optimizers = ['SGD', 'MomentumSGD', 'AdaGrad', 'RMSProp', 'Adam']
 
-# 定义非凸函数
-def h(x, y):
-    return x**2 - y**2
+## 优化h(x)
+init_point = [0.1, 0.1]
+num_steps = 50
+histories = {}
+for optimizer_name in optimizers:
+    histories[optimizer_name] = optimize_function_with_optimizers(h, optimizer_name, lr, num_steps, init_point)
+plot_optimization_comparison(histories, h, plot_type='surface', save_gif=True, gif_path='Result/Non_convex/optimization_h_lr001.gif')
 
-# 创建三维网格
-x = np.linspace(-2, 2, 100)
-y = np.linspace(-2, 2, 100)
-X, Y = np.meshgrid(x, y)
-Z = h(X, Y)
+## 优化非凸有极小值的函数g(x)
+init_point = [0., -2.]
+num_steps = 500
 
-# 设置迭代次数
-num_iterations = 50
+# lr = 0.01
+lr = 0.01
+histories = {}
+seed_everything()
+for optimizer_name in optimizers:
+    histories[optimizer_name] = optimize_function_with_optimizers(g, optimizer_name, lr, num_steps, init_point)
+plot_optimization_comparison(histories, g, plot_type='contour', save_gif=True, gif_path='Result/Non_convex/optimization_g_lr001.gif')
 
-# 初始化参数和优化器
-def create_optimizer(opt_name, initial_point, lr=0.01):
-    param = torch.tensor(initial_point, requires_grad=True, device=device)
-    if opt_name == 'SGD':
-        optimizer = SGDOptimizer([param], lr=lr)
-    elif opt_name == 'MomentumSGD':
-        optimizer = MomentumSGDOptimizer([param], lr=lr, momentum=0.9)
-    elif opt_name == 'AdaGrad':
-        optimizer = AdagradOptimizer([param], lr=lr)
-    elif opt_name == 'RMSProp':
-        optimizer = RMSPropOptimizer([param], lr=lr)
-    elif opt_name == 'Adam':
-        optimizer = AdamOptimizer([param], lr=lr)
-    return param, optimizer
+# AdaGrad的lr = 0.05，其余lr = 0.01
+histories = {}
+lr_AdaGrad = 0.05
+seed_everything()
+for optimizer_name in optimizers:
+    if optimizer_name == 'AdaGrad':
+        histories[optimizer_name] = optimize_function_with_optimizers(g, optimizer_name, lr_AdaGrad, num_steps, init_point)
+    else:
+        histories[optimizer_name] = optimize_function_with_optimizers(g, optimizer_name, lr, num_steps, init_point)
+plot_optimization_comparison(histories, g, plot_type='contour', save_gif=True, gif_path='Result/Non_convex/optimization_g_lr001_005.gif')
 
-# 定义优化器和颜色
-opt_names = ['SGD', 'MomentumSGD', 'AdaGrad', 'RMSProp', 'Adam']
-colors = ['red', 'green', 'blue', 'purple', 'orange']
-initial_point = [0.1, 0.1]  # 避开鞍点(0,0)
-params = {}
-optimizers = {}
-for name, color in zip(opt_names, colors):
-    params[name], optimizers[name] = create_optimizer(name, initial_point)
-
-# 为每个优化器创建独立的路径
-paths = {name: [] for name in opt_names}
-
-# 执行优化并记录每步路径
-for opt_name, optimizer in optimizers.items():
-    for i in range(num_iterations):
-        optimizer.zero_grad()
-        loss = h(params[opt_name][0], params[opt_name][1])
-        loss.backward()
-        optimizer.step()
-        paths[opt_name].append(params[opt_name].detach().clone().cpu().numpy())  # Move to CPU for plotting
-        if (i + 1) % 10 == 0:
-            print(f"Optimizer: {opt_name}, Iteration: {i+1}/{num_iterations}, Loss: {loss.item()}")
-
-# 创建图和轴
-fig = plt.figure(figsize=(10, 8))
-ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='viridis', alpha=0.5)
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Function Value')
-ax.legend()
-
-# 绘制迭代点和路径的函数
-lines = {opt_name: ax.plot([], [], [], label=opt_name, color=color)[0] for opt_name, color in zip(opt_names, colors)}
-points = {opt_name: ax.scatter([], [], [], color=color, depthshade=False) for opt_name, color in zip(opt_names, colors)}
-
-# 初始化动画函数
-def init():
-    # 设置初始视图角度
-    ax.view_init(elev=30, azim=30)  # elev 和 azim 可以根据需要调整
-    return lines.values(), points.values()
-
-# 更新动画的函数
-def update(num):
-    # 在每次更新前，重新设置视图角度以保持视图不变
-    ax.view_init(elev=30, azim=30)  # 保持与init中相同的视图角度
-    for opt_name in opt_names:
-        line = lines[opt_name]
-        point = points[opt_name]
-        path_data = np.array(paths[opt_name][:num+1])
-        if num > 0 and len(path_data) > 0:
-            xdata, ydata = path_data[:, 0], path_data[:, 1]
-            zdata = h(torch.tensor(xdata, dtype=torch.float32), torch.tensor(ydata, dtype=torch.float32))
-            line.set_data(xdata, ydata)
-            line.set_3d_properties(zdata)
-            point._offsets3d = (np.array([xdata[-1]]), np.array([ydata[-1]]), np.array([zdata[-1]]))
-    ax.legend()
-    return [line for line in lines.values()] + [point for point in points.values()]
-
-# 创建动画
-ani = FuncAnimation(fig, update, frames=num_iterations, init_func=init, blit=False, repeat=False)
-
-# 保存动画
-gif_path = 'Result/Non_convex/optimization_h.gif'
-ani.save(gif_path, writer='pillow', fps=10)
-print(f"The optimization paths GIF has been saved at '{gif_path}'.")
+# AdaGrad的lr = 0.1，其余lr = 0.01
+histories = {}
+lr_AdaGrad = 0.1
+seed_everything()
+for optimizer_name in optimizers:
+    if optimizer_name == 'AdaGrad':
+        histories[optimizer_name] = optimize_function_with_optimizers(g, optimizer_name, lr_AdaGrad, num_steps, init_point)
+    else:
+        histories[optimizer_name] = optimize_function_with_optimizers(g, optimizer_name, lr, num_steps, init_point)
+plot_optimization_comparison(histories, g, plot_type='contour', save_gif=True, gif_path='Result/Non_convex/optimization_g_lr001_01.gif')
